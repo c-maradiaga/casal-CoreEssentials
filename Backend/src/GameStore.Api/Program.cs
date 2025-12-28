c01bce35a7a53fa3f8bd779c1d57a5e26bf22ff4
Carlos Maradiaga <carlos.maradiaga@hotmail.com>
2025-12-28 00:09:37 -0600

    Hasta Adding the data model

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,5 +1,35 @@
+using GameStore.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<Game> games = new()
+{
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "The Witcher 3: Wild Hunt",
+        Genre = "RPG",
+        Price = 39.99M,
+        ReleaseDate = new DateOnly(2015, 5, 19)
+    },
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "Cyberpunk 2077",
+        Genre = "Action RPG",
+        Price = 59.99M,
+        ReleaseDate = new DateOnly(2020, 12, 10)
+    },
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "Minecraft",
+        Genre = "Sandbox",
+        Price = 26.95M,
+        ReleaseDate = new DateOnly(2011, 11, 18)
+    }
+};
 
 app.MapGet("/", () => "Hello World!");
 
